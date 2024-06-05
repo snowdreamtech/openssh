4224c1d4fe782b9d3bc84fcee048bffc6c4d7113
snowdream <sn0wdr1am@qq.com>
2024-06-05 16:44:20 +0800

    feat: ssh-keygen

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-
-if [ ! -f "ssh_host_rsa_key" ];then
-  ssh-keygen -A
+# ssh-keygen -A
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ];then
+  ssh-keygen -A > /dev/null 2>&1
 fi
 
 # fastfetch from https://github.com/fastfetch-cli/fastfetch
@@ -11,6 +11,14 @@ fastfetch --pipe false > /etc/motd 2>&1
 
 # change the password for root
 echo "root:$SSH_ROOT_PASSWORD" | chpasswd > /dev/null 2>&1
+
+# generate ssh keys
+if [ ! -d "/root/.ssh" ];then
+  ssh-keygen -t ed25519 -C "user@example.com" -f $HOME/.ssh/id_ed25519 -q -N ""
+  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f $HOME/.ssh/id_rsa -q -N ""
+  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f $HOME/.ssh/id_ecdsa -q -N ""
+  ssh-keygen -t dsa  -C "user@example.com" -f $HOME/.ssh/id_dsa -q -N ""
+fi
 
 # start sshd
 /usr/sbin/sshd -D
