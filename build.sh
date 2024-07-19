6557127178138dd17436904cd74072486c148a6c
snowdream <sn0wdr1am@qq.com>
2024-07-19 23:55:40 +0800

    feat: shell format

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ DOCKER_HUB_PROJECT=snowdreamtech/openssh
 GITHUB_PROJECT=ghcr.io/snowdreamtech/openssh
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:9.7 \
--t ${DOCKER_HUB_PROJECT}:9 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:9.7 \
--t ${GITHUB_PROJECT}:9 \
-. \
---push
+    -t ${DOCKER_HUB_PROJECT}:latest \
+    -t ${DOCKER_HUB_PROJECT}:9.7 \
+    -t ${DOCKER_HUB_PROJECT}:9 \
+    -t ${GITHUB_PROJECT}:latest \
+    -t ${GITHUB_PROJECT}:9.7 \
+    -t ${GITHUB_PROJECT}:9 \
+    . \
+    --push
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,22 @@
 set -e
 
 # ssh-keygen -A
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ];then
-  ssh-keygen -A > /dev/null 2>&1
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+  ssh-keygen -A >/dev/null 2>&1
 fi
 
 # fastfetch from https://github.com/fastfetch-cli/fastfetch
-fastfetch --pipe false > /etc/motd 2>&1
+fastfetch --pipe false >/etc/motd 2>&1
 
 # change the password for root
-echo "root:$SSH_ROOT_PASSWORD" | chpasswd > /dev/null 2>&1
+echo "root:$SSH_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
 
 # generate ssh keys
-if [ ! -d "/root/.ssh" ];then
+if [ ! -d "/root/.ssh" ]; then
   ssh-keygen -t ed25519 -C "user@example.com" -f $HOME/.ssh/id_ed25519 -q -N ""
   ssh-keygen -t rsa -b 4096 -C "user@example.com" -f $HOME/.ssh/id_rsa -q -N ""
   ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f $HOME/.ssh/id_ecdsa -q -N ""
-  ssh-keygen -t dsa  -C "user@example.com" -f $HOME/.ssh/id_dsa -q -N ""
+  ssh-keygen -t dsa -C "user@example.com" -f $HOME/.ssh/id_dsa -q -N ""
 fi
 
 # start sshd
