c31525159618fd77310c269810baa953da3dac0b
snowdream <sn0wdr1am@qq.com>
2024-09-28 23:07:45 +0800

    feat: openssh  based on alpine and debian

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -24,4 +24,14 @@ fi
 /usr/sbin/sshd -D
 
 # exec commands
-exec "$@"
+if [ -n "$*" ]; then
+    sh -c "$*"
+fi
+
+# keep the docker container running
+# https://github.com/docker/compose/issues/1926#issuecomment-422351028
+if [ "${KEEPALIVE}" -eq 1 ]; then
+    trap : TERM INT
+    tail -f /dev/null & wait
+    # sleep infinity & wait
+fi
