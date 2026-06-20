f2b50dfdc0f41dab96fc2734653a2c1a36c484c3
snowdream <snowdream@users.noreply.github.com>
2026-06-21 00:05:36 +0800

    fix(docker): ensure explicit permissions for client ssh keys generation

--- docker/debian/entrypoint.d/20-openssh-start.sh
+++ docker/debian/entrypoint.d/20-openssh-start.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 set -e
 
-# ssh-keygen -A
+# Generate a random password for root if not provided
+if [ -z "${SSH_ROOT_CRED}" ]; then
+    SSH_ROOT_CRED=$(openssl rand -base64 33)
+    echo "Generate random ssh root password: ${SSH_ROOT_CRED}"
+fi
+
+# Change the password for root
+echo "root:$SSH_ROOT_CRED" | chpasswd >/dev/null 2>&1
+
+# Generate host keys if not present
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
   ssh-keygen -A >/dev/null 2>&1
 fi
 
-# openssl rand -base64 33
-if [ -z "${SSH_ROOT_CRED}" ]; then {
-    SSH_ROOT_CRED=$(openssl rand -base64 33)
-    echo "Generate random ssh root password:  ${SSH_ROOT_CRED}"
-}
-fi
-
-# change the password for root
-echo "root:$SSH_ROOT_CRED" | chpasswd >/dev/null 2>&1
-
-# generate ssh keys
+# Generate ssh client keys
 if [ ! -d "/root/.ssh" ]; then
-  ssh-keygen -t ed25519 -C "user@example.com" -f "$HOME"/.ssh/id_ed25519 -q -N ""
-  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f "$HOME"/.ssh/id_rsa -q -N ""
-  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f "$HOME"/.ssh/id_ecdsa -q -N ""
+  mkdir -p /root/.ssh
+  chmod 700 /root/.ssh
+  ssh-keygen -t ed25519 -C "user@example.com" -f /root/.ssh/id_ed25519 -q -N ""
+  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f /root/.ssh/id_rsa -q -N ""
+  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f /root/.ssh/id_ecdsa -q -N ""
 fi
