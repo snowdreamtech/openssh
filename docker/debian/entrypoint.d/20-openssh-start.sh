24c2b71eef260edccfa2b35a855994361a0f95fd
snowdream <snowdream@users.noreply.github.com>
2026-06-21 00:32:59 +0800

    fix(docker): remove redundant client ssh keys generation from entrypoint

--- docker/debian/entrypoint.d/20-openssh-start.sh
+++ docker/debian/entrypoint.d/20-openssh-start.sh
@@ -15,11 +15,3 @@ if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
   ssh-keygen -A >/dev/null 2>&1
 fi
 
-# Generate ssh client keys
-if [ ! -d "/root/.ssh" ]; then
-  mkdir -p /root/.ssh
-  chmod 700 /root/.ssh
-  ssh-keygen -t ed25519 -C "user@example.com" -f /root/.ssh/id_ed25519 -q -N ""
-  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f /root/.ssh/id_rsa -q -N ""
-  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f /root/.ssh/id_ecdsa -q -N ""
-fi
